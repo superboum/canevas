4d063e4754d500874869857102aca21d089cc2b3
Quentin Dufour <quentin@dufour.tk>
2013-12-10 09:00:56 +0100

    Update db

--- db.sql
+++ db.sql
@@ -1,7 +1,14 @@
 --
 -- MySQL 5.5.33a
--- Mon, 09 Dec 2013 21:01:47 +0000
+-- Tue, 10 Dec 2013 08:00:12 +0000
 --
+
+CREATE TABLE `cdb_mailing` (
+   `id` int(10) not null auto_increment,
+   `email` varchar(255) not null,
+   PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+
 
 CREATE TABLE `cdb_people` (
    `id` int(10) not null auto_increment,
@@ -16,11 +23,6 @@ CREATE TABLE `cdb_people` (
    `answer` text,
    `notes` text,
    `member` varchar(255),
-   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
-
-CREATE TABLE `cdb_mailing` (
-   `id` int(10) not null auto_increment,
-   `email` varchar(255) not null,
+   `school` varchar(255),
    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
